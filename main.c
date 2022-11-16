d12e92883adfe7f880072608a222f56b515dbcce
Rafael Daniel <faeldaniel@gmail.com>
2022-11-16 08:56:38 -0300

    Update main.c
    Function name correction!

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
 */
-unsigned short checkSprtAround(short OBJ1X,  short OBJ1Y,  short OBJ2X, short OBJ2Y, unsigned short size){
+unsigned short checkObjAround(short OBJ1X,  short OBJ1Y,  short OBJ2X, short OBJ2Y, unsigned short size){
 	if (
 		((OBJ1Y == OBJ2Y) & ((OBJ1X > OBJ2X) & ((OBJ1X - size) <= OBJ2X))) |
 		((OBJ1Y == OBJ2Y) & ((OBJ1X < OBJ2X) & ((OBJ1X + size) >= OBJ2X))) |
